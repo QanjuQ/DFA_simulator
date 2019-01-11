b74a3f3513b6c40821af22a52d6b8672d5ca73e4
Anjum Qureshi <anjumqureshi000@gmail.com>
2019-01-12 01:23:48 +0530

    Refactor the machine_test

--- test/machine_test.js
+++ test/machine_test.js
@@ -4,35 +4,32 @@ const isInstanceOfState = require('../src/machine.js').isInstanceOfState;
 const chai = require('chai');
 
 describe("Machine", () => {
+  let machine, state;
+  beforeEach(() => {
+    machine = new Machine();
+    state = new State('q0');
+  });
   describe("transit", () => {
-    it("Should return the state when input is present in alphabets", () => {
-      let state = new State('q0');
+    beforeEach(() => {
       state.addTransitState(0, 'q1');
       state.addTransitState(1, 'q2');
-      let machine = new Machine(state);
       machine.addState(state);
       machine.initialState('q0');
+    })
+    it("Should return the state when input is present in alphabets", () => {
       chai.assert.equal(machine.transit(0), 'q1');
     });
     it("Should throw error when input is not present in alphabets", () => {
-      let state = new State('q0');
-      state.addTransitState(0, 'q1');
-      state.addTransitState(1, 'q2');
-      let machine = new Machine();
-      machine.addState(new State('q1'));
-      machine.initialState('q1');
-      chai.expect(() => machine.transit(1)).to.throw(Error);
+      chai.expect(() => machine.transit(2)).to.throw(Error);
     });
   });
   describe("addState", () => {
     it("Should add state to machine", () => {
       let state = new State('qa1');
-      let machine = new Machine(state);
       machine.addState(state);
       chai.expect(machine.states).to.deep.include(state);
     });
     it("Should not add state when state is not instance of State", () => {
-      let machine = new Machine(new State('qa1'));
       const state = "q1";
       machine.addState(state);
       chai.assert.notDeepInclude(machine.states, state);
@@ -41,7 +38,7 @@ describe("Machine", () => {
 
   describe("isInstanceOfState", () => {
     it("Should return true when given input is instance of State", () => {
-      chai.assert.isTrue(isInstanceOfState(new State('qa1')));
+      chai.assert.isTrue(isInstanceOfState(state));
     });
     it("Should return false when given input is not instance of State", () => {
       chai.assert.isFalse(isInstanceOfState('qa1'));
@@ -50,15 +47,12 @@ describe("Machine", () => {
 
   describe("initialState", () => {
     it(`Should assign initial and current state when is one of the states`, () => {
-      const state = new State('q0');
-      let machine = new Machine();
       machine.addState(state);
       machine.initialState('q0');
       chai.assert.deepEqual(machine._initialState, state.name);
       chai.assert.deepEqual(machine._currentState, state.name);
     });
     it(`Should not add when state is one of the states`, () => {
-      let machine = new Machine();
       chai.expect(() => machine.initialState('q0')).to.throw(Error);
       chai.assert.isUndefined(machine._initialState);
       chai.assert.isUndefined(machine._currentState);
@@ -67,44 +61,33 @@ describe("Machine", () => {
 
   describe("getState", () => {
     it("Should return a state for given name when exists", () => {
-      let state = new State('q0');
-      let machine = new Machine();
       machine.addState(state);
       chai.expect(machine.getState('q0')).to.deep.equal(state);
     });
     it("Should return undefined for given name when doesn't exist", () => {
-      let machine = new Machine();
       chai.assert.isUndefined(machine.getState('q0'));
     });
   });
-  describe("Should return true when input is accepted", () => {
-    let q0 = new State('q0');
-    q0.addTransitState(0, 'q0');
-    q0.addTransitState(1, 'q1');
-    q0.makeAcceptable();
-    let q1 = new State('q1');
-    q1.addTransitState(0, 'q1');
-    q1.addTransitState(1, 'q1');
-    let machine = new Machine();
-    machine.addState(q0);
-    machine.addState(q1);
-    machine.initialState('q0');
-    console.log(machine);
-    chai.assert.isTrue(machine.run('0000'));
-  });
-  describe("Should return true when input is accepted", () => {
-    let q0 = new State('q0');
-    q0.addTransitState(0, 'q0');
-    q0.addTransitState(1, 'q1');
-    q0.makeAcceptable();
-    let q1 = new State('q1');
-    q1.addTransitState(0, 'q1');
-    q1.addTransitState(1, 'q1');
-    let machine = new Machine();
-    machine.addState(q0);
-    machine.addState(q1);
-    machine.initialState('q0');
-    chai.assert.isFalse(machine.run('1000'));
-  });
+  describe("run", () => {
+    beforeEach(() => {
+      let q0 = new State('q0');
+      q0.addTransitState(0, 'q0');
+      q0.addTransitState(1, 'q1');
+      q0.makeAcceptable();
+      let q1 = new State('q1');
+      q1.addTransitState(0, 'q1');
+      q1.addTransitState(1, 'q1');
+      machine.addState(q0);
+      machine.addState(q1);
+      machine.initialState('q0');
+    });
 
+    it("Should return true when input is accepted", () => {
+      chai.assert.isTrue(machine.run('0000'));
+    });
+
+    it("Should return false when input is rejected", () => {
+      chai.assert.isFalse(machine.run('1000'));
+    });
+  });
 });
